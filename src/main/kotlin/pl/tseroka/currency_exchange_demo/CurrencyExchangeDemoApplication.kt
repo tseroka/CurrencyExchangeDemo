00c22da4eeeeee0075975f1249eee61763f5d5b4
Tymon Seroka <tymonseroka@gmail.com>
2022-06-21 21:23:49 +0200

    initial commit

---
+++ src/main/kotlin/pl/tseroka/currency_exchange_demo/CurrencyExchangeDemoApplication.kt
@@ -0,0 +1,11 @@
+package pl.tseroka.currency_exchange_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CurrencyExchangeDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<CurrencyExchangeDemoApplication>(*args)
+}
---
+++ src/test/kotlin/pl/tseroka/currency_exchange_demo/CurrencyExchangeDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.tseroka.currency_exchange_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CurrencyExchangeDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
